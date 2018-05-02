fe0d46643cca94f64ad10cfad1dc2406da219613
Ricardo Brancas <anjomigel@gmail.com>
2018-05-02 15:56:47 +0100

    Updated wtex

--- bin/wtex.sh
+++ bin/wtex.sh
@@ -2,7 +2,7 @@
 
 DIR=$(dirname "$1")
 
-pdflatex -interaction=nonstopmode -halt-on-error $1
+latexrun $1
 while inotifywait -e close_write $DIR; do
-	pdflatex -interaction=nonstopmode -halt-on-error $1
+	latexrun $1
 done
